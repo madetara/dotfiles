301609ddf905d94022b0d0a8cab4145c1e6cb03e
madetara <madetara@yandex.com>
2020-01-14 20:09:29 +0300

    feat(neovim): add coc.nvim settings

--- install.sh
+++ install.sh
@@ -4,3 +4,6 @@ BASEDIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 
 # neovim
 ln -s ${BASEDIR}/neovim/init.vim ~/.config/nvim/init.vim
+
+# coc LSP plugin for neovim
+ln -s ${BASEDIR}/neovim/coc/coc-settings.json ~/.config/nvim/coc-settings.json
